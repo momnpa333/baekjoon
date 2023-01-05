aa3921dd0d97c64c318839a6316ee15222f1aa19
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-06 02:56:53 +0900

    A로 B만들기.py

---
+++ programmers/lv0/A로 B만들기.py
@@ -0,0 +1,13 @@
+before="olleh"
+after="heloo"	
+
+def solution(before, after):
+    before=sorted(before)
+    dict1={string:before.count(string) for string in before}
+    after=sorted(after)
+    dict2={string:after.count(string) for string in after}
+    if dict1 == dict2:
+        return 1
+    else:
+        return 0
+print(solution(before, after))
