d643e90e0f3a8fbd38ca9e585d4a22bbd761eb99
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-06 03:48:23 +0900

    핸드폰 번호 가리기.py

---
+++ programmers/lv1/핸드폰 번호 가리기.py
@@ -0,0 +1,9 @@
+phone_number="027778889"
+
+def solution(phone_number):
+    numberlen=len(phone_number)-4
+    answer='*'*numberlen
+    answer=answer+phone_number[-4:]
+    return answer
+
+print(solution(phone_number))
