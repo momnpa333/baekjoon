36b8f2888864c4ef3a94022a5a048e27757067fb
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-06 17:47:12 +0900

    2789.py

---
+++ bronze/2789.py
@@ -0,0 +1,9 @@
+str=input()
+solv=''
+for i in str:
+    if i =='C' or i =='A' or i =='M' or i =='B'or i =='R' or i =='I' or i =='D' or i =='G' or i =='E':
+       continue
+    else:
+        solv+=i
+print(solv)
+    
