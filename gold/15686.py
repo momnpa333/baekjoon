b2c8ea4b22201449747a9e61aaf51c04f49fdc03
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-01-30 00:18:08 +0900

    15686.py

--- gold/15686.py
+++ gold/15686.py
@@ -3,10 +3,9 @@ input=sys.stdin.readline
 
 N,M=map(int,input().split())
 city=[list(map(int,input().split()))for _ in range(N)]
-print(city)
+
 foodInfo=[]
 userInfo=[]
-foodlength=9999999
 #치킨정보 저장
 for i in range(N):
     for j in range(N):
@@ -14,13 +13,37 @@ for i in range(N):
             foodInfo.append([i,j])
         if city[i][j] == 1:
             userInfo.append([i,j])
+def foodlength(foods):
+    foodlength=9999999
+    foodsum=0
+    for user in userInfo:
+        for food in foods:
+            if foodlength>abs(user[0]-food[0])+abs(user[1]-food[1]):
+                foodlength=(abs(user[0]-food[0])+abs(user[1]-food[1]))
+        else:
+            foodsum+=foodlength
+            foodlength=9999999
+    return foodsum
+answer=[]
+def dfs(currentChicken,idx,chickenNum):
+    if chickenNum==M:
+        answer.append(foodlength(currentChicken))
+        return
+    if chickenNum>=M or idx>len(foodInfo)-1:
+        return
+    tmp=currentChicken.copy()
+    dfs(tmp,idx+1,chickenNum)
+    currentChicken.append(foodInfo[idx])
+    tmp=currentChicken.copy()
+    dfs(tmp,idx+1,chickenNum+1)
+tmp=[]
+dfs(tmp,0,0)
+print(min(answer))
 
-for user in userInfo:
-    for food in foodInfo:
-        if foodlength>abs(user[0]-food[0])+abs(user[1]-food[1]):
-            foodlength=abs(user[0]-food[0])+abs(user[1]-food[1])
 
 
 
-    
+
+
+
 
