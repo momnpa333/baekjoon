64cf31ae6d2d1e9714e9819d0d95d507b4004ccf
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-02-13 16:26:21 +0900

    네트워크.py

---
+++ programmers/lv1/네트워크.py
@@ -0,0 +1,32 @@
+from collections import Counter
+def solution(n, computers):
+    answer = 0
+    parent=[]
+    for i in range(n):
+        parent.append(i)
+    def findparent(n):
+        if parent[n]==n:
+            return n 
+        parent[n]=findparnet(parent[n])
+        return parent[n]
+    for i in range(n):
+        for j in range(i,n):
+            if i!=j and computers[i][j]==1:
+                parent[i]=min(parent[i],parent[j])
+                parent[j]=min(parent[i],parent[j])
+    for i in range(n):
+        for j in range(i,n):
+            if i!=j and computers[i][j]==1:
+                parent[i]=min(parent[i],parent[j])
+                parent[j]=min(parent[i],parent[j])
+                
+    for i in range(n):
+        for j in range(i,n):
+            if i!=j and computers[i][j]==1:
+                parent[i]=min(parent[i],parent[j])
+                parent[j]=min(parent[i],parent[j])
+    answer=(len(Counter(parent)))
+            
+            
+        
+    return answer
