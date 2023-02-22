7b7a588f08e7e3d193a365d8821e420a562d6ea7
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-02-22 16:26:51 +0900

    1806.py

--- gold/1806.py
+++ gold/1806.py
@@ -8,20 +8,29 @@ seq=list(map(int,input().split()))
 start=0
 finish=0
 sum=seq[0]
-answer=10000001
-while start!=N-1:
+answer=100000001
+while start!=N:
     if sum>=S:
         if answer>finish-start+1:
             answer=finish-start+1
+            sum-=seq[start]
+            if start==finish:
+                finish+=1
+            start+=1
         else:
             sum-=seq[start]
+            if start==finish:
+                finish+=1
             start+=1
-    if sum<S:
+    elif sum<S:
         if finish!=N-1:
             finish+=1
             sum+=seq[finish]
         else:
             break
-print(answer)
+if answer==100000001:
+    print(0)
+else:
+    print(answer)
     
 
