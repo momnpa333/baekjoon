d9abcf813d4df72d81ed684f7544ae1b5e2cf78f
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-03-07 20:26:31 +0900

    10775.py

--- gold/10775.py
+++ gold/10775.py
@@ -4,7 +4,32 @@ input=sys.stdin.readline
 G=int(input())
 P=int(input())
 gate=[i for i in range(G+1)]
+cnt=0
+flag=1
+def dfs(v):
+    global cnt
+    global flag
+    if gate[v]==0:
+        return 0
+    if v!=0 and gate[v]==v:
+        gate[v]-=1
+        cnt+=1
+        flag=2
+        return gate[v]
+    gate[v]=dfs(gate[v])
+    return gate[v]
 
-for _ in range(P):
+park=[]
+for i in range(P):
     p=int(input())
+    park.append(p)
+for p in park:
+    dfs(p)
+    if flag==1:
+        answer=cnt
+        break
+    flag=1
+print(cnt)
+
+
     
