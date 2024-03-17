80e6df0e640c7bc413c7d1a6617d72026bbe0bef
Kwon Da Woon <momnpa333@gmail.com>
2024-03-17 20:14:46 +0900

    solve 2470

--- gold/2470.py
+++ gold/2470.py
@@ -8,12 +8,13 @@ left=0;right=len(drinkAry)-1
 acid=drinkAry[left]+drinkAry[-1]
 answer=[drinkAry[left],drinkAry[right]]
 while(left<right):
+    print(left,right,drinkAry[left]+drinkAry[right])
     if abs(drinkAry[left]+drinkAry[right])<abs(acid):
         answer=[drinkAry[left],drinkAry[right]]
         acid=drinkAry[left]+drinkAry[right]
-    if acid<0:
+    if drinkAry[left]+drinkAry[right]<0:
         left+=1
-    elif acid>0:
+    elif drinkAry[left]+drinkAry[right]>0:
         right-=1
     else:
         break
