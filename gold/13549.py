034c6e10f6a15288744314480b2a75db03230232
Kwon Da Woon <momnpa333@gmail.com>
2024-03-25 16:12:09 +0900

    13549.py

--- gold/13549.py
+++ gold/13549.py
@@ -5,12 +5,16 @@ N,K=map(int,input().split(' '))
 count_board=[float('inf')]*100001
 
 def fastmove(pos,cnt,K):
+    ret=[]
     if pos==K:
         print(cnt)
         exit(0)
-    if pos==0 and count_board[pos]==float('inf'):
+    if count_board[pos]==float('inf'):
         count_board[pos]=cnt
-    # count_board[pos]=cnt
+        ret.append(pos)
+        pos*=2
+    else:
+        return ret
     if pos!=0:
         while pos<100001:
             if pos==K:
@@ -23,7 +27,9 @@ def fastmove(pos,cnt,K):
             if pos+1>K*2:
                 break
             count_board[pos]=cnt
+            ret.append(pos)
             pos*=2
+    return ret
 
 def findstart(cnt):
     ret=[]
@@ -33,18 +39,20 @@ def findstart(cnt):
     return ret
 
 dq=deque()
-fastmove(N,0,K)
-for item in findstart(0):
+next=fastmove(N,0,K)
+for item in next:
     dq.append((item,0))
 while dq:
     # print(dq)
+    next=[]
     for _ in range(len(dq)):
         start_point,cnt=dq.popleft()
         for add_point in (-1,1):
             if 0<=start_point+add_point<100001 and count_board[start_point+add_point]==float('inf'):
                 # count_board[start_point+add_point]=cnt+1
-                fastmove(start_point+add_point,cnt+1,K)
-    for item in findstart(cnt+1):
+                next+=fastmove(start_point+add_point,cnt+1,K)
+    next=sorted(next)
+    for item in next:
         dq.append((item,cnt+1))
 
 
---
+++ gold/9527.py
@@ -0,0 +1,4 @@
+A,B=map(int,input().split(' '))
+
+for i in range(A,B+1):
+    print(bin(i)[2:])
