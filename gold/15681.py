b1eadbd8b5f1d22b85776a7aa228d0cfb3028d95
momnpa333 <momnpa333@gmail.com>
2025-03-24 14:19:49 +0900

    15681.py

---
+++ gold/15681.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+N,R,Q=map(int,input().split())
+tree=[[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    S,E=map(int,input().split())
+    tree[S].append(E)
+    tree[E].append(S)
+
+dp=[0]*(N+1); check=[True]*(N+1)
+check[R]=False
+def find_child(root):
+    child_num=0
+    for child in tree[root]:
+        if check[child]==True:
+            check[child]=False
+            child_num+=find_child(child)
+    dp[root]=child_num+1
+    return dp[root]
+
+find_child(R)
+
+for _ in range(Q):
+    print(dp[int(input())])
+
+    
