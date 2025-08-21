2530faa96ebfb82a6876ece8004b2b8e2b09928d
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2025-08-21 14:21:22 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 232 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,M,L,K;
+    static int[][] stars;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        L=Integer.parseInt(st.nextToken());
+        K=Integer.parseInt(st.nextToken());
+
+        stars=new int[K][2];
+
+        for(int i=0;i<K;i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            stars[i][0]=Integer.parseInt(st.nextToken());
+            stars[i][1]=Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<K;i++){
+            for(int j=0;j<K;j++){
+                answer=Math.max(countingStar(stars[i][0],stars[j][1]),answer);
+                answer=Math.max(countingStar(stars[j][0],stars[i][1]),answer);
+            }
+        }
+        System.out.println(K-answer);
+
+    }
+
+    static int countingStar(int x,int y){
+        int ret=0;
+        int cnt=0;
+        for(int i=0;i<K;i++){
+            if(stars[i][0]>=x && stars[i][0]<=x+L && stars[i][1]>=y && stars[i][1]<=y+L){
+                cnt++;
+            }
+        }
+        ret=Math.max(ret,cnt);
+        cnt=0;
+        for(int i=0;i<K;i++){
+            if(stars[i][0]>=x-L && stars[i][0]<=x && stars[i][1]>=y && stars[i][1]<=y+L){
+                cnt++;
+            }
+        }
+
+        ret=Math.max(ret,cnt);
+        cnt=0;
+        for(int i=0;i<K;i++){
+            if(stars[i][0]>=x && stars[i][0]<=x+L && stars[i][1]>=y-L && stars[i][1]<=y){
+                cnt++;
+            }
+        }
+
+        ret=Math.max(ret,cnt);
+        cnt=0;
+        for(int i=0;i<K;i++){
+            if(stars[i][0]>=x-L && stars[i][0]<=x && stars[i][1]>=y-L && stars[i][1]<=y){
+                cnt++;
+            }
+        }
+
+        ret=Math.max(ret,cnt);
+        return ret;
+    }
+}
