0d5ea8e44d2a49ceb1a4b6e6eb97603091acbd2b
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2025-08-22 16:45:22 +0900

    [Gold II] Title: 나누기, Time: 160 ms, Memory: 24116 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] seq;
+    static long[] preSum;
+    static long[][][] dp;
+    static double tar;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N=Integer.parseInt(st.nextToken());
+        preSum=new long[N+1];
+        seq=new int[N];
+        dp=new long[5][N+1][N+1];
+
+        st=new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++){
+            seq[i]=Integer.parseInt(st.nextToken());
+            preSum[i+1]=preSum[i]+seq[i];
+        }
+        tar= (double) preSum[N] /4;
+
+//        System.out.println(Arrays.toString(preSum));
+        System.out.println(solve(1,N,4));
+    }
+    static long solve(int start,int end,int num){
+        if(start>end){
+            return 0;
+        }
+//        System.out.printf("%d %d %d\n",start,end,num);
+        if(dp[num][start][end]!=0){
+            return dp[num][start][end];
+        }
+        if(num==1){
+            dp[num][start][end]=1;
+            return 1;
+        }
+        long ret=0;
+        for(int i=start;i<=end;i++){
+            if(preSum[i]-preSum[start-1]==tar){
+                ret+=solve(start,i,1)*solve(i+1,end,num-1);
+            }
+        }
+        return ret;
+    }
+
+}
