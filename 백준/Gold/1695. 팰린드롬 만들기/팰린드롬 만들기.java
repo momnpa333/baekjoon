7d92d2ae8f7a85cf34c4ad11c58713f4f71dffe3
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2025-12-17 10:21:47 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 192 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -3,47 +3,38 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[] seq;
-    static int[] reverseSeq;
-    static int[][] dp;
-    static int ans;
+    static int[] seq, rev;
+    static int[] dp;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
 
-        N=Integer.parseInt(st.nextToken());
-        seq=new int[N+1];
-        reverseSeq=new int[N+1];
-        st=new StringTokenizer(br.readLine()," ");
-        for(int i=1;i<=N;i++){
-            seq[i]=Integer.parseInt(st.nextToken());
-            reverseSeq[N-i+1]=seq[i];
+        seq = new int[N + 1];
+        rev = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+            rev[N - i + 1] = seq[i];
         }
-        dp=new int[N+1][N+1];
-//        System.out.println(Arrays.toString(seq));
-//        System.out.println(Arrays.toString(reverseSeq));
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=N;j++){
-//                System.out.println(seq[i]+" "+reverseSeq[j]);
-                if(seq[i]==reverseSeq[j]){
-                    dp[i][j]=Math.max(dp[i-1][j-1]+1,dp[i-1][j-1]);
+
+        dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            int prev = 0; 
+            for (int j = 1; j <= N; j++) {
+                int temp = dp[j]; 
+                if (seq[i] == rev[j]) {
+                    dp[j] = prev + 1;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-1]);
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i][j]);
-                }
+                prev = temp;
             }
         }
-//        for(int i=0;i<=N;i++){
-//            System.out.println(Arrays.toString(dp[i]));
-//        }
-        for(int j=1;j<=N;j++){
-            ans=Math.max(dp[N][j],ans);
-        }
 
-        System.out.println(N-ans);
+        int lps = dp[N];
+        System.out.println(N - lps);
     }
 }
-
-
