ca794533f4325a37e1506088ea308f7d44110519
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2026-01-15 09:29:04 +0900

    [Gold III] Title: 짠돌이 호석, Time: 284 ms, Memory: 58128 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N1,M1;
+    static int N2,M2;
+    static boolean[][] mat1;
+    static boolean[][] mat2;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N1=Integer.parseInt(st.nextToken());
+        M1=Integer.parseInt(st.nextToken());
+        mat1=new boolean[N1][M1];
+
+        for(int i=0;i<N1;i++){
+            String s=br.readLine();
+            for(int j=0;j<M1;j++){
+                if(s.charAt(j)=='1'){
+                    mat1[i][j]=true;
+                }
+                else {
+                    mat1[i][j]=false;
+                }
+            }
+//            System.out.println(Arrays.toString(mat1[i]));
+        }
+
+        st= new StringTokenizer(br.readLine(), " ");
+        N2=Integer.parseInt(st.nextToken());
+        M2=Integer.parseInt(st.nextToken());
+        mat2=new boolean[N2][M2];
+
+        for(int i=0;i<N2;i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M2; j++) {
+                if (s.charAt(j) == '1') {
+                    mat2[i][j] = true;
+                } else {
+                    mat2[i][j] = false;
+                }
+            }
+//            System.out.println(Arrays.toString(mat2[i]));
+        }
+        turnLeft(mat2);
+
+        int ans=Integer.MAX_VALUE;
+
+        for(int i=0;i<4;i++){
+            mat2=turnLeft(mat2);
+            for(int r=0;r<N1;r++){
+                for(int c=0;c<M1;c++){
+                    ans=Math.min(ans, overLap(mat1,mat2,r,c));
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+    static int overLap(boolean[][] matA, boolean[][] matB, int startR, int startC){
+        int R=Math.max(matA.length, startR+matB.length);
+        int C=Math.max(matA[0].length, startC+matB[0].length);
+        boolean[][] newMat=new boolean[R][C];
+
+        for(int i=0;i<matA.length;i++){
+            for(int j=0;j<matA[0].length;j++){
+                newMat[i][j]=matA[i][j];
+            }
+        }
+        for(int i=0;i<matB.length;i++){
+            for(int j=0;j<matB[0].length;j++){
+                if(matB[i][j]){
+                    if(newMat[startR+i][startC+j]){
+                        return Integer.MAX_VALUE;
+                    }
+                    newMat[startR+i][startC+j]=true;
+                }
+            }
+        }
+        return R*C;
+    }
+    static boolean[][] turnLeft(boolean[][] mat){
+        int N=mat.length;
+        int M=mat[0].length;
+        boolean[][] newMat=new boolean[M][N];
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                newMat[M-1-j][i]=mat[i][j];
+            }
+        }
+        return newMat;
+    }
+}
+
