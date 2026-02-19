75ecd5c0be56dba2e90740fb0fe6122631006f68
Kwon Da woon <82216606+momnpa333@users.noreply.github.com>
2026-02-19 15:17:05 +0900

    [Gold III] Title: 알 수 없는 문장, Time: 120 ms, Memory: 15776 KB -BaekjoonHub

--- 백준/Gold/1099. 알 수 없는 문장/알 수 없는 문장.java
+++ 백준/Gold/1099. 알 수 없는 문장/알 수 없는 문장.java
@@ -2,65 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String sentence;
-    static int N;
-    static String[] words;
+    static String target;
     static int[] dp;
+    static int N;
+    static final int INF=987654321;
+    static String[] words;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-
-        sentence=br.readLine();
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        StringTokenizer st; //= new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        target=br.readLine();
+        st=new StringTokenizer(br.readLine()," ");
         N=Integer.parseInt(st.nextToken());
         words=new String[N];
-        dp=new int[sentence.length()+1];
-        Arrays.fill(dp,987654321);
         for(int i=0;i<N;i++){
             words[i]=br.readLine();
         }
-        dp[0]=0;
-//        System.out.println(Arrays.toString(words));
 
-        for(int i=0;i<=sentence.length();i++){
+        int L=target.length();
+        dp=new int[L+1];
+        Arrays.fill(dp,INF);
+        dp[0]=0;
+
+        for(int i=1;i<=L;i++){
             for(int j=0;j<N;j++){
-                if(i+words[j].length()>sentence.length()) continue;
-                if(!isPossible(sentence.substring(i,i+words[j].length()),words[j])) continue;
-                dp[i+words[j].length()]=Math.min(dp[i+words[j].length()],dp[i]+score(sentence.substring(i,i+words[j].length()),words[j]));
+                if(i-words[j].length()<0) continue;
+                if(!isPossible(target.substring(i- words[j].length(),i),words[j])) continue;
+                dp[i]=Math.min(dp[i-words[j].length()]+calc(target.substring(i-words[j].length(),i),words[j]),dp[i]);
             }
         }
-        if(dp[sentence.length()]==987654321){
+        if (dp[L] == INF) {
             System.out.println(-1);
             return;
         }
-        System.out.println(dp[sentence.length()]);
+        System.out.println(dp[L]);
     }
     static boolean isPossible(String A,String B){
-        int[] cntA=new int[30];
-        int[] cntB=new int[30];
-        Arrays.fill(cntA,0);
-        Arrays.fill(cntB,0);
+        int[] cntA=new int[26];
+        int[] cntB=new int[26];
+//        System.out.println(A+" "+B);
+
         for(int i=0;i<A.length();i++){
-            cntA[A.charAt(i)-'a']++;
-            cntB[B.charAt(i)-'a']++;
+            cntA[A.charAt(i) -'a']++;
         }
-        for(int i=0;i<30;i++){
-            if(cntA[i]!=cntB[i]) return false;
+        for(int i=0;i<B.length();i++){
+            cntB[B.charAt(i) -'a']++;
+        }
+//        System.out.println(Arrays.toString(cntA));
+//        System.out.println(Arrays.toString(cntB));
+        for(int i=0;i<26;i++){
+            if(cntB[i]>cntA[i]){
+                return false;
+            }
         }
         return true;
     }
-    static int score(String A,String B){
-//        System.out.println("!");
-//        System.out.println(A);
-//        System.out.println(B);
+    static int calc(String A,String B){
         int ret=0;
         for(int i=0;i<A.length();i++){
-            if(A.charAt(i)==B.charAt(i)){
+            if(A.charAt(i)!=B.charAt(i)){
                 ret++;
             }
         }
-//        System.out.println(A.length()-ret);
-        return A.length()-ret;
+        return ret;
     }
 }
+
